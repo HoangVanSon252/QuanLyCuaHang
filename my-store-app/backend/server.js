dd95ebea1a718e60d9983128e22f863c94822d30
Hoang Van Son <hoangvanson2552@gmail.com>
2026-07-16 23:47:30 +0700

    fix cors

--- my-store-app/backend/server.js
+++ my-store-app/backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 
 // Middleware (Khai báo cho phép đọc dữ liệu JSON gửi lên)
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  origin: true,
   credentials: true
 }));
 app.use(express.json());
